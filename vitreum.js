e8dbe8e11661cdaa3977f4beaa4562203b649732
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-02-25 15:22:24 -0500

    Added cahcebusting for server side rendering

--- vitreum.js
+++ vitreum.js
@@ -1,5 +1,6 @@
 
 module.exports = {
 	render : require('./lib/render.js'),
-	tasks : require('./lib/tasks.js')
+	tasks : require('./lib/tasks.js'),
+	cacheBusting : require('./lib/cacheBusting.js'),
 }
