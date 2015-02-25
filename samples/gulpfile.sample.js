b8d8d57bb40fac9692a2f5554be91d6353ef022f
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-02-25 16:46:13 -0500

    Adding server side rendering for static projects and updated the sample gulpfile

--- samples/gulpfile.sample.js
+++ samples/gulpfile.sample.js
@@ -4,24 +4,24 @@ var gulp = require('gulp');
 var vitreum = require("vitreum");
 
 gulp = vitreum.tasks(gulp, {
-	entryPoints: ["./client/samplePage"],
+	entryPoints: ["./client/testProj"],
 	DEV: true,
-	buildPath: "./build/",
 
+	projectType : 'STATIC',
+
+	buildPath: "./build/",
 	pageTemplate: "./client/template.hbs",
 
-	configPath : 'sample/config',
-
-	projectModules: ["./node_modules/sample", "./node_modules/palette"],
+	projectModules: ["./node_modules/testProj"],
 	assetExts: ["*.svg", "*.png", "*.jpg", "*.pdf", "*.eot", "*.ttf", "*.woff", "*.woff2"],
 
 	serverWatchPaths: ["server"],
-	serverScript: "./server/server.js",
-
+	serverScript: "./server.js",
 	cdn: {
-		"react": ["window.React", "<script src='//cdnjs.cloudflare.com/ajax/libs/react/0.10.0/react-with-addons.js'></script>"],
-		"jquery": ["window.jQuery", "<script src='//code.jquery.com/jquery-1.11.0.min.js'></script>"],
-		"lodash": ["window._", "<script src='//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js'></script>"]
+		'react/addons' : ["window.React","<script src='//cdnjs.cloudflare.com/ajax/libs/react/0.12.2/react-with-addons.min.js'></script>"],
+		jquery : ["window.jQuery","<script src='//code.jquery.com/jquery-1.11.0.min.js'></script>"],
+		lodash : ["window._","<script src='//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js'></script>"],
+
 	},
 	libs: [],
 });
