ca8e29658ef5abf5bbf56c52f454032c5ee5a85a
Kellen Steffen <kellen.steffen@thalmic.com>
2015-05-05 15:02:30 -0400

    Making head tags non-global.

--- headtags/headtags.js
+++ headtags/headtags.js
@@ -1,17 +1,18 @@
 var _ = require('lodash');
 var React = require('react');
-global.HEAD_TAGS = {};
+
+var headTags = {};
 
 module.exports = {
 	setTitle : function(titleText){
-		global.HEAD_TAGS.title = React.createElement("title", null, titleText);
+		headTags.title = React.createElement("title", null, titleText);
 		if(typeof document !== 'undefined') document.title = titleText;
 	},
 	addTags : function(tagObj){
-		global.HEAD_TAGS = _.extend(global.HEAD_TAGS, tagObj);
+		headTags = _.extend(headTags, tagObj);
 	},
 	convertToString : function(){
-		return _.reduce(global.HEAD_TAGS, function(r, tags, key){
+		return _.reduce(headTags, function(r, tags, key){
 			if(!_.isArray(tags)) tags = [tags];
 			var convertedTags = _.map(tags, function(tag){
 				return React.renderToStaticMarkup(tag);
