620e7aa481fb99f2f1e1e8f8393ce1bc617e8908
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2015-10-28 13:29:38 -0400

    updating the tasks.js with the new task structure

--- tasks/tasks.js
+++ tasks/tasks.js
@@ -15,6 +15,10 @@ module.exports = function(gulp, configOverride){
 	config.projectPaths = _.union(config.entryPoints, config.projectModules);
 
 
+	//Tasks
+	gulp.task('clean', require('./tasks/clean').bind(this, config));
+	gulp.task('libs', require('./tasks/libs').bind(this, config));
+
 	gulp.task('js', require('./tasks/js').bind(this, config, false));
 	gulp.task('less', require('./tasks/less').bind(this, config));
 
@@ -24,19 +28,55 @@ module.exports = function(gulp, configOverride){
 	});
 
 	gulp.task('template', require('./tasks/template').bind(this, config));
-	gulp.task('libs', require('./tasks/libs').bind(this, config));
 	gulp.task('assets', require('./tasks/assets').bind(this, config));
-	//gulp.task('watch', require('./tasks/watch').bind(this, gulp, config));
-	gulp.task('clean', require('./tasks/clean').bind(this, config));
-	gulp.task('runserver', require('./tasks/runserver').bind(this, config));
+
 	gulp.task('livereload', require('./tasks/livereload').bind(this, config));
+	gulp.task('runserver', require('./tasks/runserver').bind(this, config));
+
+
+
+	/** Hybrid Tasks */
+
+	gulp.task('run', function (callback){
+		runSequence(
+			'js-watch',
+			['assets','less-watch','template'],
+			'livereload','runserver',
+		callback);
+	});
+
+	gulp.task('fresh', function (callback){
+		runSequence('clean','libs','run', callback);
+	});
+
+	gulp.task('build', function (callback){
+		runSequence(
+			'clean',
+			['libs','js'],
+			['assets','less','template'],
+		callback);
+	});
+	gulp.task('prod', function(callback){
+		config.DEV = false;
+		runSequence('build', callback);
+	});
+
+	gulp.task('cmds', function(){
+		console.log('    gulp run   : assets, js-watch, less-watch, template, livereload, runserver');
+		console.log('    gulp fresh : clean, libs, run');
+		console.log('    gulp build : clean, libs, assets, js, less, template');
+		console.log('    gulp prod  : build (but with the dev flag set to false)');
+		console.log('    gulp       : run');
+	});
+
+	gulp.task('default', ['run']);
 
 
 
 
+	/**  Experimental */
 
-
-	//Triggers when a new js file is added
+	//Triggers js-watch when a new js file is added
 	gulp.task('js-new', function(){
 		watch(utils.makeBlob(config.projectPaths, ['*.js', '*.jsx'], '/**/'), {events : ['add']}, function(){
 			console.log('new file!');
@@ -46,60 +86,6 @@ module.exports = function(gulp, configOverride){
 
 
 
-
-	gulp.task('test', function(callback){
-		runSequence('clean', 'js', 'less', callback);
-	});
-
-
-	gulp.task('fresh2', function(callback){
-		runSequence('clean', 'js-watch',
-			['libs', 'assets', 'less', 'html'],
-			['livereload'], 'server', callback);
-	});
-
-	gulp.task('run2', function(callback){
-		runSequence('js-watch',
-			['less', 'html'],
-			['livereload'], 'server', callback);
-	})
-
-
-
-
-
-	gulp.task('build', function (callback) {
-		runSequence('clean', 'architecture', ['libs', 'assets', 'js', 'less', 'html'], callback);
-	});
-	gulp.task('update', function(callback){
-		runSequence(['architecture', 'assets'], 'run', callback);
-	});
-	gulp.task('run', function (callback) {
-		runSequence(['watch', 'livereload'], 'server', callback);
-	});
-	gulp.task('fresh', function(callback){
-		runSequence('clean', ['libs', 'assets', 'js', 'less', 'html'], ['watch', 'livereload'], 'server', callback);
-	})
-	gulp.task('prod', function(callback){
-		config.DEV = false;
-		runSequence('build', callback);
-	})
-
-
-
-
-
-
-	gulp.task('cmds', function(){
-		console.log('    build   : Cleans and re-builds the entire project');
-		console.log('    run     : Sets up watchers, livereload and starts the server');
-		console.log('    update  : Picks up new files, then runs the project');
-		console.log('    fresh   : Does a full build and then run');
-		console.log('    prod    : Does a full build with the DEV flag to false');
-		console.log('    default : Runs the project');
-	});
-
-	gulp.task('default', ['run']);
 	return gulp;
 };
 console.timeEnd("Loading vitreum");
