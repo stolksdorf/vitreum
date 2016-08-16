3305a92ffea8825513b2b5209d0c706290af36c9
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-08-15 22:45:57 -0400

    Added in 'jsx' global command

---
+++ bin/jsx/jsx.templates.js
@@ -0,0 +1,26 @@
+var _ = require('lodash');
+
+module.exports = {
+	jsx : (name) => {
+		var Name = _.upperFirst(name);
+		return `var React = require('react');
+var _ = require('lodash');
+var cx = require('classnames');
+
+var ${Name} = React.createClass({
+
+	render : function(){
+		return <div className='${name}'>
+			${Name} Component Ready.
+		</div>
+	}
+});
+
+module.exports = ${Name};
+`;
+	},
+	less : (name) => {
+		var Name = _.upperFirst(name);
+		return `.${name}{\n\n}`;
+	}
+}
---
+++ bin/jsx/yoyo/yoyo.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var _ = require('lodash');
+var cx = require('classnames');
+
+var Yoyo = React.createClass({
+
+	render : function(){
+		return <div className='yoyo'>
+			Yoyo Component Ready.
+		</div>
+	}
+});
+
+module.exports = Yoyo;
+
