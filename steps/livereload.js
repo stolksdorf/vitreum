bba8f9ad35eca76d22dfa35ce2e1455a3e8584bc
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-12-18 16:26:48 -0500

    livereload now has partial

--- steps/livereload.js
+++ steps/livereload.js
@@ -1,10 +1,13 @@
 const livereload = require('livereload');
 const log = require('../utils/log.js');
+const addPartial = require('../utils/partialfn.js');
 
 let lr_server;
-module.exports = () => {
+const livereload = () => {
 	log.checkProduction('livereload');
 	log.watch('live reoad running');
 	if(!lr_server) lr_server = livereload.createServer();
 	return lr_server.watch(`build`);
 };
+
+module.exports = addPartial(livereload);
