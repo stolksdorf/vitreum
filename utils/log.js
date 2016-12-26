46e8e33152ba0e430667dbc1ccd30aa633f4dc8d
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-12-26 13:36:00 -0500

    Missed an error

--- utils/log.js
+++ utils/log.js
@@ -42,7 +42,7 @@ module.exports = {
 		console.log(chalk.gray(`  ${label}, updating bundle cache...`));
 	},
 
-	jsxError : (err) => {
+	jsxError : (error) => {
 		const err = _.defaults(error,{
 			filename : '',
 			message : '',
