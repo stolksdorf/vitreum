c0799b25da3699a362aca9fba86b7fe963c6a3d7
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2016-12-26 15:50:17 -0500

    Lesswatch was not watching the shared directories

--- steps/less.watch.js
+++ steps/less.watch.js
@@ -1,5 +1,4 @@
-
-
+const _ = require('lodash');
 const storage = require('../utils/storage.js');
 const log = require('../utils/log.js');
 const addPartial = require('../utils/partialfn.js');
@@ -8,13 +7,16 @@ const LessStep = require('./less.js');
 
 const lesswatch = (name, shared=[]) => {
 	log.checkProduction('less-watch');
-
 	const chokidar  = require('chokidar');
 
-	const rootPath = storage.entryDir(name);
+	const watchPaths = _.map(shared, (sharedDir) => {
+		return `${sharedDir}/**/*.less`;
+	});
+	watchPaths.push(`${storage.entryDir(name)}/**/*.less`);
+
 	return LessStep(name, shared, storage.deps(name))
 		.then(() => {
-			chokidar.watch(`${rootPath}/**/*.less`, {ignoreInitial : true})
+			chokidar.watch(watchPaths, {ignoreInitial : true})
 				.on('change', ()=>{
 					LessStep(name, shared, storage.deps(name))
 				});
