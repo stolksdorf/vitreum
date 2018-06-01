e7a1be5e942927d0ae883660c60c8a5c3659b824
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-06-01 13:46:46 -0400

    Now pulls bable config from package, caches require, and squelches errors

--- require.js
+++ require.js
@@ -1,31 +1,36 @@
 const path = require('path');
 const selfpath = path.join(__dirname, 'require.js');
 const getOpts   = require('./lib/getopts.js');
-const fse = require('fs-extra');
 const keyName = '____';
+
+let cache = {};
 
 if(process.argv[1] != selfpath){
 	const child_process = require('child_process');
 	const vm = require('vm');
 	return module.exports = (targetPath)=>{
 		const fullTargetPath = path.resolve(path.dirname(module.parent.filename), targetPath);
+		if(cache[fullTargetPath]) return cache[fullTargetPath];
 		const cmd = `node ${__dirname}/require.js ${fullTargetPath}`;
 		const compiledCode = child_process.execSync(cmd).toString();
 		const sandbox = {};
 		vm.runInNewContext(compiledCode, sandbox);
-		return sandbox[keyName];
+		cache[fullTargetPath] = sandbox[keyName];
+		return cache[fullTargetPath];
 	}
 }else{
 	const browserify = require('browserify');
 	const transform = require('./lib/transforms');
 	const targetFile = process.argv[2];
-	const opts = getOpts({app:''}, targetFile);
-	browserify(targetFile, {
+	const opts = getOpts({app:'', entry:{}}, targetFile);
+	opts.babel.compact = true;
+	browserify({
 			standalone    : keyName,
 			paths         : opts.shared,
 			ignoreMissing : true,
 		})
-		.transform((file)=>transform(file, opts))
+		.require(targetFile)
+		.transform((file)=>transform(file, opts), {global :true})
 		.bundle((err, buf)=>{
 			if(err) return process.stderr.write(err.toString());
 			process.stdout.write(buf.toString())
