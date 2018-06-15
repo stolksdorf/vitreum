1812bdb562e5e8a76e01d15baa33f8cc57cbe18c
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2018-06-15 19:13:08 -0400

    Fixing the require to use absolute paths

--- require.js
+++ require.js
@@ -1,17 +1,11 @@
 const utils      = require('./lib/utils.js');
-const path       = require('path');
 const browserify = require('browserify');
 const transform  = require('./lib/transforms');
 const getOpts    = require('./lib/getopts.js');
 
-let Cache = {};
-
-module.exports = async (targetFile, useCache=true)=>{
-	const fullTargetPath = path.resolve(path.dirname(module.parent.filename), targetFile);
+module.exports = (fullTargetPath)=>{
 	const opts = getOpts({app:'', entry:{}}, fullTargetPath);
-
 	return new Promise((resolve, reject)=>{
-		if(useCache && Cache[fullTargetPath]) return resolve(Cache[fullTargetPath]);
 		browserify({
 			standalone    : '___',
 			paths         : opts.shared,
@@ -24,8 +18,7 @@ module.exports = async (targetFile, useCache=true)=>{
 		.bundle((err, buf)=>{
 			if(err) return reject(err);
 			const code = `(function() { ${buf.toString()}\nreturn module.exports;}())`;
-			Cache[fullTargetPath] = eval(code);
-			return resolve(Cache[fullTargetPath]);
+			return resolve(eval(code));
 		});
 	})
 };
