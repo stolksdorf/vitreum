697fb914aa1fbf0eaf1284ef3ae4521ae6933e59
Scott Tolksdorf <scott.tolksdorf@gmail.com>
2020-10-19 09:30:00 -0400

    Including recipes within npm install

--- utils/postinstall.js
+++ utils/postinstall.js
@@ -2,7 +2,12 @@ const chalk = require('./chalk.js');
 const pckg = require('../package.json');
 const peerDeps = Object.keys(pckg.peerDependencies).join(' ');
 
-console.log(chalk.green('\nBe sure to install the needed peer dependencies:'));
+console.log('');
+console.log('');
+console.log(chalk.green('Be sure to install the needed peer dependencies:'));
 console.log(chalk.cyan(`npm install ${peerDeps}`));
 
-console.log(`\nUse ${chalk.cyan(`node node_modules/vitreum/utils/scaffold.js`)} to quickly setup your project.`)
+console.log('');
+console.log(`Use ${chalk.cyan(`node node_modules/vitreum/utils/scaffold.js`)} to quickly setup your project.`)
+console.log('');
+console.log('');
