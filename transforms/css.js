3b96cd54184a7e643454d2b1a76657d83d2a1a59
removedporn <86824510+removedporn@users.noreply.github.com>
2021-08-24 23:29:25 +0800

    Delete transforms directory

--- transforms/css.js
+++
@@ -1,9 +0,0 @@
-let cache = {}
-const transform = (code, fp, opts)=>{
-	cache[fp]=code;
-}
-//TODO: figure out how to make sourcemaps for generic css
-//https://github.com/floridoo/concat-with-sourcemaps
-transform.generate = ()=>Object.values(cache).join('\n');
-
-module.exports = transform;
